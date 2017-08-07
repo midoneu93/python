8bb8656524e84fae39316f761d5363c0e30ff90c
midoneu93 <mohamedabuelgheit101@gmail.com>
2017-08-07 03:06:42 +0200

    classes

---
+++ classes.py
@@ -0,0 +1,22 @@
+class calculator :
+
+    def addition(x, y):
+        added = x + y
+        print(added)
+
+    def subtraction(x, y):
+        sub = x - y
+        print(sub)
+
+    def multiplication(x, y):
+        mult = x * y
+        print(mult)
+
+    def division(x, y):
+        div = x / y
+        print(div)
+
+calculator.addition(10, 5)
+calculator.subtraction(10, 5)
+calculator.multiplication(10, 5)
+calculator.division(10, 5)
