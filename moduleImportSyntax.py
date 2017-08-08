3794804a3135ed5a9fc7048f30e45b2d6a887fcb
midoneu93 <mohamedabuelgheit101@gmail.com>
2017-08-08 03:34:28 +0200

    module import

--- moduleImportSyntax.py
+++ moduleImportSyntax.py
@@ -1,5 +1,10 @@
 from statistics import mean as me , median as med
 
+#from statistics import *
+#mean = mean(example_list)
+#median = median(example_list)
+
+
 example_list = [3, 5, 4, 6, 4, 87, 9, 7, 6, 3, 10]
 
 mean = me(example_list)
