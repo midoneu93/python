d62f97d3bfce28099d2a8489a95dea28947be782
midoneu93 <mohamedabuelgheit101@gmail.com>
2017-08-08 03:17:17 +0200

    some statistics

---
+++ py_statistics.py
@@ -0,0 +1,12 @@
+import statistics
+
+example_list = [3, 5, 4, 6, 4, 87, 9, 7, 6, 3, 10]
+
+mean = statistics.mean(example_list)
+print(mean)
+
+median = statistics.median(example_list)
+print(median)
+
+stdev = statistics.stdev(example_list)
+print(stdev)
